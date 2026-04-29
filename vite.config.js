9fbb7d90afe34447f03b0a6570cf5404c76cda9c
Nishant <nishant@example.com>
2026-04-29 09:59:10 +0530

    Trigger_redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Trigger redeploy after security off
 export default defineConfig({
+
   plugins: [react()],
   build: {
     rolldownOptions: {
